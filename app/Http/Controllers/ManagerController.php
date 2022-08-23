2a0248f04ae99507f28f32aa2accc2acd06afa06
fransdika <70425775+fransdika@users.noreply.github.com>
2022-08-23 14:53:59 +0700

    misterkong

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Misterkong;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\Misterkong;
 
 class ManagerController extends Controller
 {
@@ -20,53 +20,53 @@ class ManagerController extends Controller
         $inserts = [];
         $data_manager = [];
         $data_company = [];
-        if(substr($nohp, 0,1) == "0"){
-            $hp = '62'.substr(trim($nohp), 1);
-        }else{
+        if (substr($nohp, 0, 1) == "0") {
+            $hp = '62' . substr(trim($nohp), 1);
+        } else {
             $hp = $phone;
         }
         $select = DB::table("m_userx")->join('m_user_company', 'm_user_company.kd_user', '=', 'm_userx.id')->select('m_user_company.kd_user', 'm_userx.nama', 'm_user_company.alamat', 'm_userx.no_hp', 'm_user_company.kd_bank', 'm_user_company.no_rek', 'm_user_company.nama_pemilik_rekening', 'm_user_company.id')->where('m_userx.no_hp', '=', $hp);
-        if($select->count() > 0){
-        $row = $select->get();
-        foreach($row as $exe) {
-            $data_manager = ['user_id' => $exe->kd_user, 'nama_pengguna'=> $data_pegawai['nama'], 'alamat' => $data_pegawai['alamat'], 'no_hp' => $hp, 'kd_bank' => $exe->kd_bank, 'no_rek' => $exe->no_rek, 'nama_pemilik_rekening' => $exe->nama_pemilik_rekening, 'no_aktif' => $hp, 'jenis' => $jenis, 'status' => 1];
-        }
-        DB::beginTransaction();
-        try {
-            if(empty($data_user['passweb'])){
-                $inserts = ['kd_group' => 1, 'nama' => $data_user['nama'], 'keterangan' => '-', 'no_hp' => $hp, 'status_phone' => 1, 'email' => $email, 'status_email' => 0, 'status' => 1];
-            }else {
-                $inserts = ['kd_group' => 1, 'nama' => $data_user['nama'], 'passwd' => $data_user['passweb'], 'keterangan' => '-', 'no_hp' => $hp, 'status_phone' => 1, 'email' => $email, 'status_email' => 0, 'status' => 1];
+        if ($select->count() > 0) {
+            $row = $select->get();
+            foreach ($row as $exe) {
+                $data_manager = ['user_id' => $exe->kd_user, 'nama_pengguna' => $data_pegawai['nama'], 'alamat' => $data_pegawai['alamat'], 'no_hp' => $hp, 'kd_bank' => $exe->kd_bank, 'no_rek' => $exe->no_rek, 'nama_pemilik_rekening' => $exe->nama_pemilik_rekening, 'no_aktif' => $hp, 'jenis' => $jenis, 'status' => 1];
             }
-            $insert = DB::table('m_userx')->updateOrInsert(['no_hp' => $hp], $inserts);
-            $manager = DB::table('m_user_manager')->updateOrInsert(['no_hp' => $hp], $data_manager);
-            $productId = DB::getPdo()->lastInsertId();
-            foreach($select as $exe) {
-                $data_company = ['id_company_id' => $exe->id, 'id_user_manager' => 1, 'status' => 1, 'keterangan' => '-'];
+            DB::beginTransaction();
+            try {
+                if (empty($data_user['passweb'])) {
+                    $inserts = ['kd_group' => 1, 'nama' => $data_user['nama'], 'keterangan' => '-', 'no_hp' => $hp, 'status_phone' => 1, 'email' => $email, 'status_email' => 0, 'status' => 1];
+                } else {
+                    $inserts = ['kd_group' => 1, 'nama' => $data_user['nama'], 'passwd' => $data_user['passweb'], 'keterangan' => '-', 'no_hp' => $hp, 'status_phone' => 1, 'email' => $email, 'status_email' => 0, 'status' => 1];
                 }
-            $company = DB::table('m_user_manager_company')->updateOrInsert(['id_user_manager' => $productId], $data_company);
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollback();
-        }
-        return response()->json(['Sucess'=>'TRUE', 'User_baru' => 'False']);
-       }else{
-        $getid = DB::table('m_userx')->select('id')->orderBy('id','desc')->first()->id+1;
-        if(empty($data_user['passweb'])){
-            $insertss = ['kd_group' => 1, 'nama' => $data_user['nama'], 'keterangan' => '-', 'no_hp' => $hp, 'status_phone' => 1, 'email' => $email, 'status_email' => 0, 'status' => 1];
-        }else {
-            $insertss = ['kd_group' => 1, 'nama' => $data_user['nama'], 'passwd' => $data_user['passweb'], 'keterangan' => '-', 'no_hp' => $hp, 'status_phone' => 1, 'email' => $email, 'status_email' => 0, 'status' => 1];
-        }
-        $insert = DB::table('m_userx')->updateOrInsert(['no_hp' => $hp], $insertss);
-            if($insert == TRUE){
-                $data_manager = ['user_id' => $getid, 'nama_pengguna'=> $data_pegawai['nama'], 'alamat' => $data_pegawai['alamat'], 'no_hp' => $hp, 'kd_bank' => '-', 'no_rek' => '-', 'nama_pemilik_rekening' => '-', 'no_aktif' => $hp, 'jenis' => $jenis, 'status' => 1];
+                $insert = DB::table('m_userx')->updateOrInsert(['no_hp' => $hp], $inserts);
+                $manager = DB::table('m_user_manager')->updateOrInsert(['no_hp' => $hp], $data_manager);
+                $productId = DB::getPdo()->lastInsertId();
+                foreach ($select as $exe) {
+                    $data_company = ['id_company_id' => $exe->id, 'id_user_manager' => 1, 'status' => 1, 'keterangan' => '-'];
+                }
+                $company = DB::table('m_user_manager_company')->updateOrInsert(['id_user_manager' => $productId], $data_company);
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollback();
+            }
+            return response()->json(['Sucess' => 'TRUE', 'User_baru' => 'False']);
+        } else {
+            $getid = DB::table('m_userx')->select('id')->orderBy('id', 'desc')->first()->id + 1;
+            if (empty($data_user['passweb'])) {
+                $insertss = ['kd_group' => 1, 'nama' => $data_user['nama'], 'keterangan' => '-', 'no_hp' => $hp, 'status_phone' => 1, 'email' => $email, 'status_email' => 0, 'status' => 1];
+            } else {
+                $insertss = ['kd_group' => 1, 'nama' => $data_user['nama'], 'passwd' => $data_user['passweb'], 'keterangan' => '-', 'no_hp' => $hp, 'status_phone' => 1, 'email' => $email, 'status_email' => 0, 'status' => 1];
+            }
+            $insert = DB::table('m_userx')->updateOrInsert(['no_hp' => $hp], $insertss);
+            if ($insert == true) {
+                $data_manager = ['user_id' => $getid, 'nama_pengguna' => $data_pegawai['nama'], 'alamat' => $data_pegawai['alamat'], 'no_hp' => $hp, 'kd_bank' => '-', 'no_rek' => '-', 'nama_pemilik_rekening' => '-', 'no_aktif' => $hp, 'jenis' => $jenis, 'status' => 1];
                 DB::beginTransaction();
                 try {
                     $manager = DB::table('m_user_manager')->updateOrInsert(['no_hp' => $hp], $data_manager);
                     $productId = DB::getPdo()->lastInsertId();
-                    foreach($select as $exe) {
+                    foreach ($select as $exe) {
                         $data_company = ['id_company_id' => $productId, 'id_user_manager' => 1, 'status' => 1, 'keterangan' => '-'];
-                        }
+                    }
                     $company = DB::table('m_user_manager_company')->updateOrInsert(['id_user_manager' => $productId], $data_company);
 
                     DB::commit();
@@ -74,68 +74,66 @@ class ManagerController extends Controller
                     DB::rollback();
                 }
             }
-        return response()->json(['Sucess'=>'TRUE', 'User_baru' => 'TRUE']);
-       }
-
-       
-   }
-   public function array_converter($array)
-   {
-    $Y = [];
-    if(!empty($array)){
-        $update = str_replace($array[0], '', $array);
-        $exe = explode(',', substr($update, 0, -1));
-        foreach($exe as $data) {
-            $x = explode('=', $data);
-            $Y[trim($x[0])] = $x[1];
+            return response()->json(['Sucess' => 'TRUE', 'User_baru' => 'TRUE']);
         }
-    }
-    return $Y;
-   }
 
-   public function login(Request $request)
-   {
-    $respon = [];
-    $data = [];
-    $input = $request->mn;
-    $password = $request->dp;
-    $where = ['input' => $input, 'passwd' => $password, 'status' => 1];
-    $res = strpos($input, '@');
-    $where['loginby'] = ($res == false) ? "phone" : "email";
-    $model = new Misterkong();
-    // dd(\DB::getQueryLog());
-    $respon = $model->login($where);
-    // dd(\DB::getQueryLog());
-    return response()->json($respon);
-
-   }
-
-   public function pencarian(Request $request)
-   {
-    $company_id = $request->company_id;
-    $phone = $request->no_hp;
-    if(substr($phone, 0,1) == "0"){
-        $hp = '62'.substr(trim($phone), 1);
-    }else{
-        $hp = $phone;
     }
-    $array = [];
-    $array = [$hp, $company_id];
-    // DB::enableQueryLog();
-    $prosedures = DB::select('call checkUserByPhone(?,?)', $array);
-    // dd(\DB::getQueryLog());
-    foreach($prosedures as $exe) {
-        $data = $exe->result;
-    }
-    if (!empty($data)) {
-        if($data == 2){
-            return response()->json(['Sucess'=>'TRUE', 'message' => 'Telah Digunakan', 'result' => $data, 'data' => $prosedures]);
-        }else{
-            return response()->json(['Sucess'=>'TRUE', 'message' => 'Sudah Terdaftar', 'result' => $data, 'data' => $prosedures]);
+    public function array_converter($array)
+    {
+        $Y = [];
+        if (!empty($array)) {
+            $update = str_replace($array[0], '', $array);
+            $exe = explode(',', substr($update, 0, -1));
+            foreach ($exe as $data) {
+                $x = explode('=', $data);
+                $Y[trim($x[0])] = $x[1];
+            }
         }
-    }else{
-        return response()->json(['Sucess'=>'TRUE', 'message' => 'blum terdaftar', 'result' => 0, 'data' => []]);
+        return $Y;
     }
-    
-   }
+
+    public function login(Request $request)
+    {
+        $data = [];
+        $input = $request->mn;
+        $password = $request->dp;
+        $where = ['input' => $input, 'passwd' => $password, 'status' => 1];
+        $res = strpos($input, '@');
+        $where['loginby'] = ($res == false) ? "phone" : "email";
+        $model = new Misterkong();
+        // dd(\DB::getQueryLog());
+        $respon = $model->login($where);
+        // dd(\DB::getQueryLog());
+        return response()->json($respon);
+
+    }
+
+    public function pencarian(Request $request)
+    {
+        $company_id = $request->company_id;
+        $phone = $request->no_hp;
+        if (substr($phone, 0, 1) == "0") {
+            $hp = '62' . substr(trim($phone), 1);
+        } else {
+            $hp = $phone;
+        }
+        $array = [];
+        $array = [$hp, $company_id];
+        // DB::enableQueryLog();
+        $prosedures = DB::select('call checkUserByPhone(?,?)', $array);
+        // dd(\DB::getQueryLog());
+        foreach ($prosedures as $exe) {
+            $data = $exe->result;
+        }
+        if (!empty($data)) {
+            if ($data == 2) {
+                return response()->json(['Sucess' => 'TRUE', 'message' => 'Telah Digunakan', 'result' => $data, 'data' => $prosedures]);
+            } else {
+                return response()->json(['Sucess' => 'TRUE', 'message' => 'Sudah Terdaftar', 'result' => $data, 'data' => $prosedures]);
+            }
+        } else {
+            return response()->json(['Sucess' => 'TRUE', 'message' => 'blum terdaftar', 'result' => 0, 'data' => []]);
+        }
+
+    }
 }
--- app/Models/Misterkong.php
+++ app/Models/Misterkong.php
@@ -2,19 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
 class Misterkong extends Model
 {
-    use HasFactory;
     public function login($data)
     {
         if ($data['loginby'] == "phone") {
             if (substr($data['input'], 0, 2) == "62" || substr($data['input'], 0, 3) == "+62") {
                 $data['input'] = $data['input'];
-            }elseif(substr($data['input'], 0, 1) == "0"){
+            } elseif (substr($data['input'], 0, 1) == "0") {
                 $data['input'][0] = "X";
                 $data['input'] = str_replace("X", "62", $data['input']);
             }
@@ -26,51 +24,69 @@ class Misterkong extends Model
             if ($data['loginby'] == "phone") {
                 $where_cek_status = ["no_hp" => $data['input'], "status_phone" => 1, "status" => $data['status']];
                 $data_login_pass = ["no_hp" => $data['input'], "passwd" => $data['passwd']];
-            }else{
+            } else {
                 $where_cek_status = ["email" => $data['input'], "status_email" => 1, "satatus" => $data['status']];
                 $data_login_pass = ["email" => $data['input'], "passwd" => $data['passwd']];
             }
             $cek_akun_status = DB::table('m_userx')->where($where_cek_status)->count();
             if ($cek_akun_status > 0) {
-                $cek = DB::table('v_getCompanyUser')->select(array(DB::raw('count(*) as aggregate')))->where($data_login_pass)->count();
-                if ($cek > 0) {
-                    if ($cek != 1) {
+                // dd(\DB::getQueryLog());
+                // $cek = DB::table('v_getCompanyUser')->select(array(DB::raw('count(*) as aggregate')))->where($data_login_pass)->count();
+                $cek = DB::select($this->getDataTable('v_getCompanyUser', $data_login_pass));
+                // dd(\DB::getQueryLog());
+                if (count($cek) > 0) {
+                    if (count($cek) != 1) {
                         $sub = DB::table('m_userx')->select('id')->where('email', '=', $data['input'])->Where('passwd', '=', $data['passwd']);
                         $query = DB::table('m_userx')->select('id')->where('no_hp', '=', $data['input'])->Where('passwd', '=', $data['passwd']);
                         $ex_compid = DB::table('m_user_company')->select('company_id', 'nama_usaha', 'alamat')->where($sub)->orWhere($query);
                         $jml_data = $ex_compid->count();
-                        if($jml_data > 0){
+                        if ($jml_data > 0) {
                             $company = ["error" => 0, "usaha" => $jml_data, "company" => []];
                             foreach ($ex_compid->get() as $value) {
-                               $det = ["company_id" => $value['ccompany_id'],
-                                        "nama_usaha" => $value['nama_usaha'],
-                                        "alamat" => $value['alamat']];
+                                $det = ["company_id" => $value['company_id'],
+                                    "nama_usaha" => $value['nama_usaha'],
+                                    "alamat" => $value['alamat']];
                                 array_push($company['company'], $det);
                             }
                             array_push($data1, $company);
-                        }else {
+                        } else {
                             $company = ["error" => 3, "usaha" => 0, "pesan" => "Tidak ada Data usaha"];
                         }
                         return $data1;
-                    }else {
-                        $company = ["error" => 0, "usaha" => 0, "pesan" => "Tidak ada Data usaha"];
+                    } else {
+                        $det = [
+                            "company_id" => $cek[0]->company_id,
+                            "nama_usaha" => $cek[0]->nama_usaha,
+                            "alamat" => $cek[0]->alamat_usaha,
+                        ];
+                        $company = ["error" => 0, "usaha" => 1, "company" => $det];
                         array_push($data1, $company);
                         return $data1;
+
                     }
-                }else {
+                } else {
                     $company = ["error" => 1, "pesan" => "No.hp atau password salah"];
-					array_push($data1, $company);
-					return $data1;
+                    array_push($data1, $company);
+                    return $data1;
                 }
-            }else {
+            } else {
                 $company = ["error" => 2, "pesan" => "Akun Anda belum terverifikasi, apakah anda ingin " . $pesan . "?", "loginby" => $data['loginby']];
                 array_push($data1, $company);
                 return $data1;
             }
-        }else {
+        } else {
             $company = ["error" => 1, "pesan" => "User atau Password salah"];
             array_push($data1, $company);
-			return $data1;
+            return $data1;
         }
+    }
+
+    public function getDataTable($table, $where)
+    {
+        foreach ($where as $key => $value) {
+            $dataWhere[] = "$key='$value'";
+        }
+        $sql = "SELECT * FROM $table WHERE " . implode(" AND ", $dataWhere);
+        return $sql;
     }
 }
