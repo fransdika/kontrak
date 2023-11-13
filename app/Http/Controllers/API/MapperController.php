3e64130eaa74572eb713d4df539b1af872a2b7cb
Dika <fransdika94@gmail.com>
2023-11-13 11:49:16 +0800

    d

--- app/Http/Controllers/API/MapperController.php
+++ app/Http/Controllers/API/MapperController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\MapperModel;
+use DB;
 
 class MapperController extends Controller
 {
@@ -33,37 +34,56 @@ class MapperController extends Controller
     }   
     public function updatePembayaran(Request $request)
     {
-        $data_save=[
-            'nominal'=>$request->nominal,
-            'awal'=>$request->awal,
-            'periode'=>$request->periode,
-            'bukti_bayar'=>$request->bukti_bayar,
-            'tanggal_bayar'=>date('Y-m-d H:i:s'),
-            'tarif_mapper_id'=>date('Y-m-d H:i:s'),
-        ];
-        $condition=[
-            'id'=>$request->id
-        ];
-        $update=MapperModel::updatePembayaran($data_save,$condition);
-        if($update){
-            $response=[
-                'status'=>1,
-                'error' =>200,
-                'message'=>'berhasil',
-                'data'=>[]
+        $data_cid=DB::table('misterkong_mapper.m_other_company_mapper')->where(['id'=>$request->id])->first();
+    
+    	if(!empty($data_cid)){
+            if (!file_exists("../../../public_html/back_end_mp/" . $data_cid->company_id . "_config/images/mapper")) {
+                mkdir("../../../public_html/back_end_mp/" . $data_cid->company_id . "_config/images/mapper", 0777, true);
+            }
+
+            $file = $request->file('file');
+            $filename = $file->getClientOriginalName();
+            $file->move('../../../public_html/back_end_mp/'.$data_cid->company_id."_config/images/mapper",$filename);
+
+            $data_save=[
+                'nominal'=>$request->nominal,
+                'awal'=>$request->awal,
+                'periode'=>$request->periode,
+                'bukti_bayar'=>$file->getClientOriginalName(),
+                'tanggal_bayar'=>date('Y-m-d H:i:s'),
+                'tarif_mapper_id'=>date('Y-m-d H:i:s'),
             ];
-            return response()->json($response,200);
+            $condition=[
+                'id'=>$request->id
+            ];
+            $update=MapperModel::updatePembayaran($data_save,$condition);
+            if($update){
+                $response=[
+                    'status'=>1,
+                    'error' =>200,
+                    'message'=>'berhasil',
+                    'data'=>[]
+                ];
+                return response()->json($response,200);
+            }else{
+                $response=[
+                    'status'=>0,
+                    'error' =>500,
+                    'message'=>'gagal',
+                    'data'=>[]
+                ];
+                return response()->json($response,500);
+            }
         }else{
-            $response=[
+        	$response=[
                 'status'=>0,
                 'error' =>500,
-                'message'=>'gagal',
+                'message'=>'wrong company id
+                ',
                 'data'=>[]
             ];
             return response()->json($response,500);
-        }
-        
-        
+        }   
     }
 
 }
