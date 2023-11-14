6a130bae8c9f138abc5e1c41bc6c65a66c8cef48
hanaramdhani <hanawesave@gmail.com>
2023-11-14 11:36:38 +0800

    up

--- app/Http/Controllers/API/Api_all.php
+++ app/Http/Controllers/API/Api_all.php
@@ -1370,7 +1370,7 @@ class Api_all extends Controller
         INNER JOIN misterkong_$request->company_id.m_kategori m_kategori ON m_barang.kd_kategori = m_kategori.kd_kategori
         INNER JOIN misterkong_$request->company_id.m_barang_satuan m_barang_satuan ON m_barang.kd_barang = m_barang_satuan.kd_barang
         INNER JOIN misterkong_$request->company_id.m_satuan m_satuan ON m_barang_satuan.kd_satuan = m_satuan.kd_satuan
-        LEFT JOIN misterkong_$request->company_id.m_barang_gambar m_barang_gambar ON m_barang.kd_barang = m_barang_gambar.kd_barang) a $query_search");
+        LEFT JOIN (SELECT kd_barang, GROUP_CONCAT(gambar) AS gambar FROM misterkong_$request->company_id.m_barang_gambar GROUP BY kd_barang) m_barang_gambar ON m_barang.kd_barang = m_barang_gambar.kd_barang) a $query_search");
 
         if ($sql && $sql2) {
             return response()->json($this->crudResponses(1,$crud_type,$sql,$sql2[0]->jumlah_record));
